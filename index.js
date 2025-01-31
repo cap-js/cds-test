bc967e794a4377a6bb6641237ad30bde6d550fb6
Christian Georgi <christian.georgi@sap.com>
2025-01-31 15:23:48 +0100

    New copy from sap/cds, as lib

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const cds = module.exports = require('@sap/cds')
-
-class CdsTest {
-
-  get test() {
-    return super.test = require('./lib/cds-test')
-  }
-
-}
-
-cds.extend (cds.constructor) .with (CdsTest)
+module.exports = require('./lib/cds-test')
