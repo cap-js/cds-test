d760fcc3a0c1b7c8a18d1b312ef57ec1f52a0760
Bob den Os <bob.den.os@sap.com>
2025-03-14 10:42:25 +0100

    Allow java apps to use cds-test

--- test/app/test/sample-bookshop.test.js
+++ test/app/test/sample-bookshop.test.js
@@ -2,11 +2,17 @@ const cds_test = require ('../../..')
 const describe = global.describe ?? require('node:test').describe
 
 describe('Sample tests', () => {
-  const { GET, expect } = cds_test (__dirname+'/..')
+  const { GET, expect, cds } = cds_test (__dirname+'/..')
 
   it('serves Books', async () => {
-    const { data } = await GET `/odata/v4/catalog/Books`
+    const { data } = await GET`/odata/v4/catalog/Books`
     expect(data.value.length).to.be.greaterThanOrEqual(5)
+  })
+
+  it('database Books', async () => {
+    const { Books } = cds.entities('sap.capire.bookshop')
+    const data = await cds.ql`SELECT ID FROM ${Books}`
+    expect(data.length).to.be.greaterThanOrEqual(5)
   })
 
 })
