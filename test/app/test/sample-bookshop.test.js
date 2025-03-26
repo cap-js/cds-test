2792b4e6ef51eec4ba38ce3e04afcba9de306ca0
Daniel Hutzel <daniel.hutzel@sap.com>
2025-03-26 14:29:42 +0100

    Replaced another global require

--- test/app/test/sample-bookshop.test.js
+++ test/app/test/sample-bookshop.test.js
@@ -2,7 +2,7 @@ const cds_test = require('../../../lib/cds-test')
 const describe = global.describe ?? require('node:test').describe
 
 describe('Sample tests', () => {
-  const { GET, expect } = cds_test('@cap-js/test-sample-app')
+  const { GET, expect } = cds_test(__dirname+'/..')
 
   it('serves Books', async () => {
     const { data } = await GET`/odata/v4/catalog/Books`
