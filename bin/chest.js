54e9e85b40a577c8eb48159e505491e994f288cd
Daniel Hutzel <daniel.hutzel@sap.com>
2025-03-28 06:26:32 +0100

    feat: using excludes from jest.configs

--- bin/chest.js
+++ bin/chest.js
@@ -11,9 +11,11 @@ const options = {
   'recent':   { type:'boolean' },
   'passed':   { type:'boolean' },
   'failed':   { type:'boolean' },
-  'match':    { type:'string', default: '(.test.js|.spec.js)$' },
-  'skip':     { type:'string', default: '^(gen,*.tmp)$' },
-  'only':     { type:'string'},
+  'match':    { type:'string' },
+  'include':  { type:'string', short:'i', default: '(.test.js|.spec.js)$' },
+  'exclude':  { type:'string', short:'x', _default: '^(gen,*.tmp)$' },
+  'only':     { type:'string', short:'o', },
+  'skip':     { type:'string', short:'n', },
   'workers':  { type:'string', short:'w' },
   'debug':    { type:'string' },
   'help':     { type:'boolean', short:'h' },
@@ -27,6 +29,10 @@ Usage:
 Options:
 
   -l, --list     List found test files
+  -i, --include  Include matching files, default: ${options.include.default}
+  -x, --exclude  Exclude matching files, default: ${options.exclude.default}
+  -n, --skip     Skip all matching test cases
+  -o, --only     Run only matching test cases
   -q, --quiet    No output at all
   -s, --silent   No output
   -t, --timeout  in milliseconds
@@ -34,19 +40,19 @@ Options:
   -v, --verbose  Increase verbosity
   -w, --workers  Specify number of workers
   -?, --help     Displays this usage info
-
-  --match        Pick matching files, default: ${options.match.default}
-  --skip         Skip matching files, default: ${options.skip.default}
-  --only         Run only the matching tests
   --failed       Repeat recently failed test suite(s)
   --passed       Repeat recently passed test suite(s)
   --recent       Repeat recently run test suite(s)
+  --match        Alias to --include
 `
 
 const { DIMMED, YELLOW, GRAY, RESET } = require('./colors')
 const regex4 = s => !s ? null : RegExp (s.replace(/[,.*]/g, s => ({ ',': '|', '.': '\\.', '*': '.*' })[s]))
 const recent = () => {try { return require(home+'/.cds-test-recent.json') } catch {/* egal */}}
 const os = require('os'), home = os.userInfo().homedir
+const path = require('node:path')
+const fs = require('node:fs')
+const { exists } = require('@sap/cds/lib/utils')
 
 async function test (argv,o) {
   if (o.help || argv == '?') return console.log (USAGE)
@@ -61,25 +67,27 @@ async function test (argv,o) {
     execArgv: [ '--require', require.resolve('../lib/fixtures/node-test.js') ],
     timeout: +o.timeout || undefined,
     concurrency: +o.workers || true,
+    forceExit: true,
+    testSkipPatterns: regex4 (o.skip), skip: false,
     testNamePatterns: regex4 (o.only), only: false,
   })
   require('./reporter')(test, test.options = o)
 }
 
 async function fetch (argv,o) {
+  if (o.match) o.include = o.match; o.exclude ??= jest().exclude || '^(gen,*.tmp)$'
   const patterns = regex4 (argv.join('|')) || { test: ()=> true }
-  const tests = regex4 (o.match || options.match.default) || { test: ()=> true }
-  const skip = regex4 (o.skip || options.skip.default) || { test: ()=> false }
+  const include = regex4 (o.include || options.include.default) || { test: ()=> true }
+  const exclude = regex4 (o.exclude || options.exclude.default) || { test: ()=> false }
   const ignore = /^(\..*|node_modules|_out)$/
   const files = []
-  const fs = require('node:fs'), path = require('node:path')
   const _read = fs.promises.readdir
   const _isdir = x => fs.statSync(x).isDirectory()
   await async function _visit (dir) {
     const entries = await _read (dir)
     return Promise.all (entries.map (each => {
-      if (ignore.test(each) || skip.test(each = path.join (dir,each))) return
-      if (tests.test(each)) return patterns.test(each) && files.push(each)
+      if (ignore.test(each) || exclude.test(each = path.join (dir,each))) return
+      if (include.test(each)) return patterns.test(each) && files.push(each)
       if (_isdir(each)) return _visit (each)
     }))
   } (process.cwd())
@@ -97,6 +105,20 @@ function list (files) {
   console.log(GRAY, time+'s', RESET, '\n')
 }
 
+function jest() {
+  const config_js = process.cwd() + '/jest.config'
+  const exists = fs.existsSync
+  for (const ext of [ '.js', '.json', '.mjs', '.cjs' ]) {
+    if (exists (config_js+ext)) {
+      // IMPORTANT: We need to jun that in a separate process to avoid loading the cds.env in current process
+      const { stdout } = require('node:child_process').spawnSync (
+        'node',  [ '-e', `console.log(require('${config_js+ext}').testPathIgnorePatterns?.join('|')||'')` ]
+      , { encoding: 'utf-8' })
+      return { exclude: stdout }
+    }
+  }
+  return {}
+}
 
 if (!module.parent) {
   const { positionals, values } = require('node:util').parseArgs ({ options, allowPositionals: true })
@@ -106,8 +128,8 @@ if (!module.parent) {
 else module.exports = Object.assign ( test, {
   options: [
     '--files',
-    '--match',
-    '--skip',
+    '--include',
+    '--exclude',
     '--timeout',
     '--workers',
   ],
@@ -121,6 +143,6 @@ else module.exports = Object.assign ( test, {
     '--passed',
     '--failed',
   ],
-  shortcuts: [ '-f', null, null, '-t', '-w', '-v', '-u', '-s', '-q', '-l' ],
+  shortcuts: [ '-f', '-i', '-x', '-t', '-w', '-v', '-u', '-s', '-q', '-l' ],
   help: USAGE
 })
