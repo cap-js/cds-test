05fbc07107b57c94e6a20c5d16dde04108b91476
Daniel Hutzel <daniel.hutzel@sap.com>
2025-07-20 01:59:51 +0200

    fix: it.each() only uses the first entry of nested arrays (#42)
    * fix: it.each() only uses the first entry of nested arrays
    
    * ..
    
    * adjusting workflow ci.yml
    
    * fixing node-test

--- test/expect.test.js
+++ test/expect.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+if (typeof describe === 'undefined') ({ describe, it } = require('node:test'))
 const expect = require('../lib/expect')
 
 describe (`supported chai features subset ...`, ()=>{
