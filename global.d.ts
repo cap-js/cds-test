d4727ef35657040d37847c8ecf3e58499be6d503
Daniel O'Grady <daniel.o-grady@sap.com>
2025-09-11 11:56:04 +0200

    Types

--- global.d.ts
+++ global.d.ts
@@ -1,25 +1,30 @@
 import type { mock } from 'node:test'
+import type each_type from './lib/fixtures/test-each.js'
 
 declare global {
     // when extending global, only var can be used
     var describe: {
-        each: () => void;
+        each: typeof each_type;
         skip: {
             each: () => void;
         }
-    } | (() => void);
+    };
     var xdescribe: typeof describe['skip'];
     var it: {
-        each: () => void,
+        each: typeof each_type,
         skip: () => void
     }
     var test: typeof it;
     var xtest: typeof it['skip'];
+    function before(method: Function): void;
     function before(message: string & Function?, method: Function): void;
-    function beforeEach(): void
+    function beforeEach(): void;
+    function beforeAll(method: Function): void;
     function beforeAll(message: string & Function?, method: Function): void;
+    function after(method: Function): void;
     function after(message: string & Function?, method: Function): void;
     function afterEach(): void;
+    function afterAll(method: Function): void;
     function afterAll(message: string & Function?, method: Function): void;
     function expect(): void;
 
