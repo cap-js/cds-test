d59a5f8fb76bc032db5d619970ef3b491f9c74ea
I548646 <paul.erlenwein@sap.com>
2026-04-29 10:06:28 +0200

    chore/gen: iterate on workflows

--- test/java-app/test/sample-java.test.js
+++ test/java-app/test/sample-java.test.js
@@ -1,7 +1,9 @@
-describe("Java integration", () => {
-  if (!/java/.test(process.env.CDS_ENV)) return it.skip("skipped", () => {});
+const cds_test = require("../../../lib/cds-test");
 
-  const cds_test = require("../../../lib/cds-test");
+describe("Java integration", () => {
+  if (!/java/.test(process.env.CDS_ENV))
+    return it("skipped in profile node", () => {});
+
   const {
     GET,
     POST,
